901bc6118b46a53123177fe5f93437c64eb8167c
VirajPasindu <virajpasindu308@gmail.com>
2024-12-24 11:59:14 +0530

    2nd

--- calculator.py
+++ calculator.py
@@ -2,4 +2,10 @@ print("1 - Add")
 print("2 - Substract")
 print("3 - Multiply")
 print("4 - Divide")
-print("choose an operation: ")
+option = int(input("choose an operation: "))
+
+if(option in [1,2,3,4]):
+    num1 = int(input("Enter first number: "))
+    
+else:
+    print("Invalid operation entered")

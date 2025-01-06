679685cb4913dd7d69272a3b0d3291f4b9471b80
VirajPasindu <virajpasindu308@gmail.com>
2025-01-06 20:58:11 +0530

    6 th

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ print("4 - Divide")
 option = int(input("choose an operation: "))
 
 if(option in [1,2,3,4]):
-    num1 = int(input("Enter first number: "))
+    num1 = float(input("Enter first number: "))
     num2 = int(input("Enter seccond number: "))
     
     if(option == 1):
@@ -15,7 +15,7 @@ if(option in [1,2,3,4]):
     elif(option == 3):
         result = num1 * num2
     elif(option == 4):
-        result = num1 // num2
+        result = num1 / num2
 
     
 else:

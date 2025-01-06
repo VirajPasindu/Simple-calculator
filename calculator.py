8107c1e42b2381849019c289272edc144da63124
VirajPasindu <virajpasindu308@gmail.com>
2025-01-06 20:56:42 +0530

    5 th

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ option = int(input("choose an operation: "))
 
 if(option in [1,2,3,4]):
     num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter first number: "))
+    num2 = int(input("Enter seccond number: "))
     
     if(option == 1):
         result = num1 + num2
@@ -20,3 +20,5 @@ if(option in [1,2,3,4]):
     
 else:
     print("Invalid operation entered")
+    
+print("The result of the operation is {}".format(result))

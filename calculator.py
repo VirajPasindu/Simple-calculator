cab3d73411543af18cd8f4b3a9fa2035ac2a2400
VirajPasindu <virajpasindu308@gmail.com>
2025-01-06 20:51:33 +0530

    4th

--- calculator.py
+++ calculator.py
@@ -6,6 +6,17 @@ option = int(input("choose an operation: "))
 
 if(option in [1,2,3,4]):
     num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter first number: "))
+    
+    if(option == 1):
+        result = num1 + num2
+    elif(option == 2):
+        result = num1 - num2
+    elif(option == 3):
+        result = num1 * num2
+    elif(option == 4):
+        result = num1 // num2
+
     
 else:
     print("Invalid operation entered")
